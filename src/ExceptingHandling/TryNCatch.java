28ecd4385891f276f5599a6ee615a95aa1c05c72
archa <archa@DESKTOP-PV8OE5S>
2022-09-13 11:42:04 +0530

    Third commit- Facing issue

--- src/ExceptingHandling/TryNCatch.java
+++ src/ExceptingHandling/TryNCatch.java
@@ -3,6 +3,8 @@ public class TryNCatch {
  
 	public static void main(String[] args) {		
 		System.out.println("No such element found exception");
+		System.out.println("Facing Issue");
+		
 	}
 	
 	}
